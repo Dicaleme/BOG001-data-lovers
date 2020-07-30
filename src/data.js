ec0a95016e27707fc5f62fff1e053502c5545765
Dani0210 <alejandrad087@gmail.com>
2020-07-30 15:57:10 -0500

    Filtros por categorias

--- src/data.js
+++ src/data.js
@@ -3,35 +3,103 @@
 import lol from './data/lol/lol.js';
 
 const campeones = Object.values(lol.data);
-console.log(campeones);
 
-export const list=()=>{
-
-  return  campeones;
-}  
-
+//Filtrado por tanque
 
 const isTank = (tanque)=>{
   return tanque.tags.includes('Tank');
 }
 export  const dataFilter = (campeonesRecibidos,rol)=>{
  let datafiltrada = [];
+ console.log(rol);
 if (rol === 'Tank'){
   datafiltrada= campeonesRecibidos.filter(isTank);
+
 }
-  console.log(datafiltrada);
   return datafiltrada;
 }
-
 //DEJAR EN MAIN.JS
-dataFilter(campeones,'Tank');
+//Filtrado por Asesinos
+const isAssassin = (asesinos)=>{
+  return asesinos.tags.includes('Assassin');
+}
+export  const filterAssassin = (campeonesRecibidos,rol)=>{
+ let datafiltrada = [];
+ console.log(rol);
+if (rol === 'Assassin'){
+  datafiltrada= campeonesRecibidos.filter(isAssassin);
+}
+ 
+  return datafiltrada;
+}
+filterAssassin(campeones,'Assassin');
 
+//Filtrado por Luchadores
+const isFighter = (luchadores)=>{
+  return luchadores.tags.includes('Fighter');
+}
+export const filterFighter = (campeonesRecibidos,rol)=>{
+ let datafiltrada = [];
+if (rol === 'Fighter'){
+  datafiltrada= campeonesRecibidos.filter(isFighter);
+}
+  
+  return datafiltrada;
+}
+filterFighter(campeones,'Fighter');
 
+//Filtrado por Magos
+const isMage = (magos)=>{
+  return magos.tags.includes('Mage');
+}
+export  const filterMage = (campeonesRecibidos,rol)=>{
+ let datafiltrada = [];
+if (rol === 'Mage'){
+  datafiltrada= campeonesRecibidos.filter(isMage);
+}
+  
+  return datafiltrada;
+}
+filterMage(campeones,'Mage');
 
+//Filtrado por  Apoyos
+const isSupport = (apoyos)=>{
+  return apoyos.tags.includes('Support');
+}
+export  const filterSupport = (campeonesRecibidos,rol)=>{
+ let datafiltrada = [];
+if (rol === 'Support'){
+  datafiltrada= campeonesRecibidos.filter(isSupport);
+}
+ 
+  return datafiltrada;
+}
+filterSupport(campeones,'Support');
 
+//Filtrado por  Tiradores
+const isMarksman = (tiradores)=>{
+  return tiradores.tags.includes('Marksman');
+}
+export  const filterMarksman = (campeonesRecibidos,rol)=>{
+ let datafiltrada = [];
+if (rol === 'Marksman'){
+  datafiltrada= campeonesRecibidos.filter(isMarksman);
+}
+ 
+  return datafiltrada;
+} 
+filterMarksman(campeones,'Marksman');
 
-/* 
-for(let i = 0;i<campeones.length;i++){
-  const campe = campeones[i];
-  console.log(`${campe.tags}`)
-}  */
+//Filtrado todos
+const isEveryone = (todos)=>{
+  return todos.tags.includes('Marksman','Support','Mage','Fighter','Assassin','Tank');
+}
+export  const filterEveryone = (campeonesRecibidos,rol)=>{
+ let datafiltrada = [];
+if (rol === 'Marksman','Support','Mage','Fighter','Assassin','Tank'){
+  datafiltrada= campeonesRecibidos.filter(isEveryone);
+}
+ 
+  return datafiltrada;
+} 
+filterMarksman(campeones,'Marksman','Support','Mage','Fighter','Assassin','Tank');
--- src/main.js
+++ src/main.js
@@ -1,74 +1,101 @@
- import {dataFilter,list}from './data.js';  
-import lol from './data/lol/lol.js';
-// import data from './data/lol/lol.js';
-//import lol from './data/lol/lol.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {dataFilter,filterAssassin,filterFighter,filterMage,filterMarksman,filterSupport,filterEveryone} from "./data.js";
+import lol from "./data/lol/lol.js";
 
-//console.log(lol);
-
-//For in
-//pasarlo de objetos a array
-//object.values
-
-
-const campeones = list();
+const campeonesData = Object.values(lol.data);
 const contenedor = document.getElementById("charBox");
 
-campeones.forEach( element => { 
+//LIMPIAR CONTENEDOR DE LAS TARJETAS
+const pintarCampeones = (campeones) => {
+     contenedor.textContent= '';
+      campeones.forEach((element) => {
+    const championsName = document.createElement("div");
+    championsName.classList.add("championsName");
+    championsName.appendChild(document.createTextNode(element.name));
 
-   const championsName = document.createElement ("div");
-   championsName.classList.add ("championsName"); 
-   championsName.appendChild(document.createTextNode(element.name))
-       
-   const modalButton = document.createElement("div")
-         modalButton.classList.add("modalButton")   
-   
-   const championContainer = document.createElement("div");
-         championContainer.classList.add("card");   
+    const modalButton = document.createElement("div");
+    modalButton.classList.add("modalButton");
 
-   const image = document.createElement("img")
-         image.src=element.img;
-         image.classList.add("champion-image");
+    const championContainer = document.createElement("div");
+    championContainer.classList.add("card");
+
+    const image = document.createElement("img");
+    image.src = element.img;
+    image.classList.add("champion-image");
 
     championContainer.appendChild(image);
-   championsName.appendChild(championContainer);   
-   contenedor.appendChild(championsName);  
-   championsName.appendChild(modalButton);
-   modalButton.appendChild(championContainer);
-    
-   modalButton.addEventListener('click', () => {
-         modalClick (element)
-   });   
+    championsName.appendChild(championContainer);
+    contenedor.appendChild(championsName);
+    championsName.appendChild(modalButton);
+    modalButton.appendChild(championContainer);
+
+    modalButton.addEventListener("click", () => {
+      modalClick(element);
+    });
+  });
+};
+pintarCampeones(campeonesData);
+
+function modalClick(element) {
+  document.querySelector("#modalTitle").textContent = element.title;
+  document.querySelector("#modalSplash").src = element.splash;
+  document.querySelector("#modalBlurb").textContent = element.blurb;
+  modalBg.classList.add("bgActive");
+}
+
+let modalBg = document.querySelector(".modalBg");
+let modalClose = document.querySelector(".modalClose");
+
+modalClose.addEventListener("click", function () {
+  modalBg.classList.remove("bgActive");
 });
-/*insertar informacion en el Modal*/
-   const modal = document.getElementById("modalContainer");
 
-function modalClick (element) {
-      /* const imageSplash = document.createElement("img")
-      imageSplash.src=element.img;
-      imageSplash.classList.add("modalSplash");
-      modal.appendChild(imageSplash);
-
-      console.log (imageSplash) */
-
-      const infoModal = document.getElementById ("modal")     
-      /*infoModal.appendChild(document.createTextNode(element.title))*/
-      
-      document.querySelector ("#modalTitle").textContent = element.title;
-      document.querySelector ("#modalSplash").src= element.splash; 
-      document.querySelector ("#modalBlurb").textContent = element.blurb;
-      /* document.querySelector ("#modalInfo").info.attack; */
-      modalBg.classList.add('bgActive');
-
-      
-} 
+// DATOS FILTRADOS
+const filtraTankes =()=>{
+      const prueba = dataFilter(campeonesData,'Tank');
+      console.log(prueba);
+      pintarCampeones(prueba);
+}
+document.getElementById("buttonTanks").addEventListener("click",filtraTankes);
    
-let modalBg = document.querySelector('.modalBg');
-let modalClose = document.querySelector('.modalClose');
+const filtraAsesinos =()=>{
+      const assas = filterAssassin(campeonesData,'Assassin');
+      console.log(assas);
+      pintarCampeones(assas);
+}
+document.getElementById("buttonAssassins").addEventListener("click",filtraAsesinos);
 
-modalClose.addEventListener('click', function () {
-   modalBg.classList.remove('bgActive');
-});
+const filtraLuchadores =()=>{
+      const figth= filterFighter(campeonesData,'Fighter');
+      console.log(figth);
+      pintarCampeones(figth);
+}
+document.getElementById("buttonFighters").addEventListener("click",filtraLuchadores);
 
-/* const filtered = lol.filter(lol => lol.tags =tank); 
-console.log(filtered)      */
+const filtraMagos =()=>{
+      const mag= filterMage(campeonesData,'Mage');
+      console.log(mag);
+      pintarCampeones(mag);
+}
+document.getElementById("buttonMagicians").addEventListener("click",filtraMagos);
+
+const filtraTiradores =()=>{
+      const tira= filterMarksman(campeonesData,'Marksman');
+      console.log(tira);
+      pintarCampeones(tira);
+}
+document.getElementById("buttonShooters").addEventListener("click",filtraTiradores);
+
+const filtraApoyos =()=>{
+      const apo = filterSupport(campeonesData,'Support');
+      console.log(apo);
+      pintarCampeones(apo);
+}
+document.getElementById("buttonProps").addEventListener("click",filtraApoyos);
+
+const filtraTodos =()=>{
+      const tod = filterEveryone(campeonesData,'Marksman','Support','Mage','Fighter','Assassin','Tank');
+      console.log(tod);
+      pintarCampeones(tod);
+}
+document.getElementById("buttonEveryone").addEventListener("click",filtraTodos);
+
